c33a9b2fad510a0d83a3fb742a5b5ef102b6d053
방준혁 <qkd3001@naver.com>
2024-05-28 15:09:42 +0900

    7주미션

---
+++ mission7.sql
@@ -0,0 +1,10 @@
+create table if not exists users
+(
+    id         int auto_increment constraint `PRIMARY`
+			primary key,
+    username   varchar(50)                         not null,
+    email      varchar(100)                        not null,
+    created_at timestamp default CURRENT_TIMESTAMP null
+);
+
+
