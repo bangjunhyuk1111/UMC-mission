34e6dfa549715001e3ddc8c0186a56731e0ed66c
방준혁 <qkd3001@naver.com>
2024-06-02 21:03:59 +0900

    flag 수정

--- mission8/src/dtos/temp.response.dto.js
+++
@@ -1,9 +0,0 @@
-// temp.response.dto.js
-
-export const tempResponseDTO = (data) => {
-    return {"testString" : data};
-}
-
-export const flagResponseDTO = (flag) => {
-    return {"flag" : flag};
-}
